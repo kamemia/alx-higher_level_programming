f52f2188cf00ecf37cdc6f153cb0868bc13cf414
kamemia <kamemia18@yahoo.com>
2021-09-09 16:34:36 +0000

    Fixed error

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import variable_load_5
-    print('{}'.format(variable_load_5.a))
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
